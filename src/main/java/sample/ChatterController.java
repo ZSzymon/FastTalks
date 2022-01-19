a2266dc1e83d7ce0aab133cb3f02a2ec6af0a585
Szymon Zywko <szymonzywko@gmail.com>
2022-01-19 15:45:56 +0100

    Fix UI imports

--- src/main/java/sample/ChatterController.java
+++ src/main/java/sample/ChatterController.java
@@ -2,15 +2,9 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
-
-import javafx.stage.Stage;
 
 import java.io.IOException;
 
