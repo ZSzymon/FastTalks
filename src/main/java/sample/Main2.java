6114627b4956b8de9a84abc68abbc9f33bbde36a
Szymon Zywko <szymonzywko@gmail.com>
2022-02-05 11:38:43 +0100

    Working Chat.

---
+++ src/main/java/sample/Main2.java
@@ -0,0 +1,40 @@
+package sample;
+import client.Client;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.util.Pair;
+
+import java.io.IOException;
+
+public class Main2 extends Application {
+    private Stage primaryStage;
+    public static Client clientBackend;
+    public static Pair<String, String> userInfo = new Pair<>("", "");
+    static {
+        try {
+            clientBackend = new Client("localhost", 8889);
+            clientBackend.connect();
+            clientBackend.startListener();
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        this.primaryStage = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("loginScene.fxml"));
+        primaryStage.setTitle("Hello World");
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
